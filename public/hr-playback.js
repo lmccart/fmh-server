e900d139937f41a0e1faf19c29a1874481503325
Lauren McCarthy <laurmccarthy@gmail.com>
2015-02-28 23:14:51 -0500

    adding clock

--- public/hr-playback.js
+++ public/hr-playback.js
@@ -21,10 +21,9 @@ $(document).ready(function() {
     var offset = new Date().getTime()%total_time;
 
     var past_date = new Date(offset + start_time);
-    console.log(past_date)
-    var mins = past_date.getMinutes();
+    var mins = past_date.getMinutes().toString();
     if (mins.length == 1) mins = '0'+mins;
-    var secs = past_date.getSeconds();
+    var secs = past_date.getSeconds().toString();
     if (secs.length == 1) mins = '0'+mins;
     
     $('#clock').html(past_date.getHours()+':'+mins+':'+secs);

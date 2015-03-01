7e04b98e0606dcc4e82401d4791c230b65e1bba2
Lauren McCarthy <laurmccarthy@gmail.com>
2015-02-28 22:28:40 -0500

    credits

--- public/hr-live.js
+++ public/hr-live.js
@@ -3,6 +3,6 @@
   function updateHR() {
     $.getJSON('https://followmyheart.herokuapp.com/get_hr', function(data) {
       $('#hr').html(data.hr);
-      console.log(data)
+      //console.log(data)
     });
   }
